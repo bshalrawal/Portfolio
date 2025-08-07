cbfc0e7e5702232a318de83d193f84ac620cf942
ukesh-dhakal <dhakalukesh90@gmail.com>
2025-08-07 12:26:56 +0545

    first

---
+++ app/Components/AboutMe.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+const AboutMe = () => {
+  return (
+   
+    <div className="max-w-4xl mx-auto my-12 p-8  ">
+      
+      
+      <h1 className="text-4xl font-bold text-center text-white underline underline-offset-4 mb-8 md:text-5xl">
+        About Me
+      </h1>
+      
+     
+      <p className="text-lg text-white leading-relaxed break-words">
+I’m Bishal Rawal, a passionate BIT student at MIT,Bagbazaar with a drive to blend creativity and technology. I currently work as a Graphic Designer at Simjung Tech, where I create visual content that aligns with modern branding standards. With a background in digital marketing from Karu Learning and Rapid Software Technologies, I bring both strategy and style to the table. I'm always eager to learn, explore new platforms, and turn innovative ideas into impactful results.      </p>
+ 
+
+    </div>
+  );
+};
+
+export default AboutMe;
